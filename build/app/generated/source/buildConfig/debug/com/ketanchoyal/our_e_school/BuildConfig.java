71afd9816ae17580a0290db037dbe0f1eeb9dfa6
Vijay Madiraju <madirajujobs@gmail.com>
2020-11-05 19:15:57 +0530

    'v1'

---
+++ build/app/generated/source/buildConfig/debug/com/ketanchoyal/our_e_school/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ketanchoyal.our_e_school;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ketanchoyal.our_e_school";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0-dev.1.0.1-alpha-20201105152328";
+}
---
+++ lib/core/Server.dart
@@ -0,0 +1,8 @@
+class Server {
+  static String baseUrl = "http://localhost1:8080/";
+  static String webApi = 'webApi/';
+  static String profileUpdate = 'profileupdate';
+  static String getProfileData = 'userdata';
+  static String postAnnouncement = 'postAnnouncement';
+  static String addAssignment = 'addAssignment';
+}
